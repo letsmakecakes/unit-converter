f9db03010a064135b57f9a4d9c955bd615124a53
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-03 00:18:47 +0530

    added main program

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"handlers"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.Static("/static", "../../web/static")
+	router.LoadHTMLGlob("../../web/templates/*.html")
+
+	// Home route (index)
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	router.POST("/length", handlers.ConvertLength)
+
+	port := "3000"
+
+	if err := router.Run(":" + port); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
+}
