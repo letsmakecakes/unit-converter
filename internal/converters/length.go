c253c3b6514766741c5e36f15029a6c1f022a80e
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-02 23:51:40 +0530

    added method to convert length

--- internal/converters/length.go
+++ internal/converters/length.go
@@ -1,1 +1,36 @@
 package converters
+
+import "errors"
+
+type LengthConverter struct {
+	FromUnit string  `json:"from_unit"`
+	ToUnit   string  `json:"to_unit"`
+	Value    float64 `json:"value"`
+}
+
+func (lc *LengthConverter) Convert() (float64, error) {
+	switch lc.FromUnit {
+	case "mm":
+		if lc.ToUnit == "cm" {
+			return lc.Value / 10, nil
+		}
+	case "cm":
+		if lc.ToUnit == "m" {
+			return lc.Value / 100, nil
+		}
+	case "m":
+		if lc.ToUnit == "km" {
+			return lc.Value / 1000, nil
+		}
+	case "ft":
+		if lc.ToUnit == "cm" {
+			return lc.Value * 30.48, nil
+		}
+	case "yard":
+		if lc.ToUnit == "mile" {
+			return lc.Value / 1760, nil
+		}
+	}
+
+	return 0, errors.New("unsupported length unit conversion")
+}
