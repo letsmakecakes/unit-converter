300453252152c0f9f1dfbe6d761e6a368f1e918e
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-03 00:19:49 +0530

    added converter interface

--- internal/converters/convert.go
+++ internal/converters/convert.go
@@ -1,1 +1,14 @@
 package converters
+
+type converter interface {
+	Convert() (float64, error)
+}
+
+func GetResult(c converter) (float64, error) {
+	value, err := c.Convert()
+	if err != nil {
+		return 0, nil
+	}
+
+	return value, err
+}
