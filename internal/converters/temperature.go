33d4d62b6e6f182ecc89ea9812be56481abbb9c4
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-03 15:44:14 +0530

    added method for temperature unit conversion

--- internal/converters/temperature.go
+++ internal/converters/temperature.go
@@ -1,1 +1,31 @@
 package converters
+
+import "errors"
+
+func ConvertTemperature(fromUnit, toUnit string, value float64) (float64, error) {
+	switch fromUnit {
+	case "C":
+		switch toUnit {
+		case "F":
+			return (value * 9 / 5) + 32, nil
+		case "K":
+			return value + 273.15, nil
+		}
+	case "F":
+		switch toUnit {
+		case "C":
+			return (value - 32) * 5 / 9, nil
+		case "K":
+			return (value-32)*5/9 + 273.15, nil
+		}
+	case "K":
+		switch toUnit {
+		case "C":
+			return value - 273.15, nil
+		case "F":
+			return (value - 273.15) * 9 / 5, nil
+		}
+	}
+
+	return 0, errors.New("unsupported temperature unit conversion")
+}
