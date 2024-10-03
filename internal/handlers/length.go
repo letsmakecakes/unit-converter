b1a54cb9987b344c721e9eff061f782a1f48f81c
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-03 14:08:47 +0530

    added necessarcy logs

--- internal/handlers/length.go
+++ internal/handlers/length.go
@@ -4,6 +4,8 @@ import (
 	"converters"
 	"net/http"
 
+	log "github.com/sirupsen/logrus"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,10 +17,16 @@ func ConvertLength(c *gin.Context) {
 		return
 	}
 
+	log.Infof("Obtained request: %+v", requestData)
+
 	result, err := converters.GetResult(requestData)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
+
+	// Use logrus for logging the result
+	log.Infof("Computed result: %v", result)
 
 	responseData := ResponseData{
 		ComputedResult: result,
