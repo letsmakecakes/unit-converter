d9ffad65498d45e48d2a0ff70c718ff8d38db376
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2024-10-03 15:04:20 +0530

    added weight unit conversion route handler

---
+++ internal/handlers/weight.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"converters"
+	"models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+func ConvertWeight(c *gin.Context) {
+	var requestData models.Request
+
+	if err := c.ShouldBindJSON(&requestData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	log.Infof("obtained request: %+v", requestData)
+
+	result, err := converters.ConvertWeight(requestData.FromUnit, requestData.ToUnit, requestData.Value)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	log.Infof("computed result: %+v", result)
+
+	responseData := models.Response{
+		ComputedResult: result,
+	}
+
+	c.JSON(http.StatusOK, responseData)
+}
